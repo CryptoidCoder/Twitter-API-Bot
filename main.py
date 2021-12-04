ef880567ca5fb0a6fb0f1fd3fe3cdadbe4823170
Josh Leach <45842827+CryptoidCoder@users.noreply.github.com>
2021-12-04 00:24:07 +0000

    Updates 2.0 (added some logging functionality)

--- main.py
+++ main.py
@@ -35,9 +35,11 @@ def checknewfollowers():
     global new_followers #make changes to new_followers globally
     newfollowerslist = None
     current_followers = list(current_followers.split(", ")) #convert string to list
+    print(f"Current Followers: {current_followers}")
     
     new_followers = functions.getfollowers(me) #get new updated list of followers
     new_followers = list(new_followers.split(", ")) #convert string to list
+    print(f"New Followers: {new_followers}")
 
     comparelists(new_followers, current_followers)
     current_followers = new_followers #make the current "new followers" list into the "old followers list"
@@ -51,7 +53,7 @@ def checknewfollowers():
 schedule.every().day.at("12:30").do(functions.tweetfromque)
 schedule.every(1).minutes.do(checknewfollowers) #every 2 minutes check if new followers
 
-print(f'Datetime Now: {datetime.now().strftime("%H:%M:%S")}')
+print(f'Starting Datetime: {datetime.now().strftime("%H:%M:%S")}')
 current_followers = functions.getfollowers(me) #save the first set of followers in a list
 while True:
     schedule.run_pending() #start all functions scheduled
